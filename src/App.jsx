e5954e7c49bcc116da67c57a803d07c1d5916237
Clive-Makongo <clivemakongo@gmail.com>
2024-08-22 18:47:08 +0100

    installed tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,17 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='bg-red-900 p-50'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className='bg-red-900'>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+        <p className='bg-red-900'>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
