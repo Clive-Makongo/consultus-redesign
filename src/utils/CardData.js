e5bea87560b3aab84759a571a46e221603425f73
Clive-Makongo <clivemakongo@gmail.com>
2024-09-17 23:33:57 +0100

    worrying about slider text

--- src/utils/CardData.js
+++ src/utils/CardData.js
@@ -1,13 +1,13 @@
 const aboutCardData = [
     {
         title: "Experience",
-        description: "We bring a wealth of experience in managing and overseeing various aspects of international and local infrastructure projects, particularly in the field of water and wastewater systems.",
+        description: "We bring a wealth of experience in managing and overseeing various aspects of international and local infrastructure projects, particularly in the field of water and wastewater systems. With a combined experience of over 20 years, we have successfully navigated the intricacies of projects falling under the umbrella of the Department of European Union Financial Assistance at the Ministry of Environment, Urbanization, and Climate Change. These projects have spanned crucial infrastructure areas such as wastewater treatment plants, drinking water treatment facilities, wastewater collector lines, wastewater pumping stations, drinking water transmission networks, solid waste landfill facilities, and more.",
         color: "bg-blue-500",
         image: "/assets/images/about-us-img1.PNG"
     },
     {
         title: "FIDIC",
-        description: "In our roles, we have acted as contract managers for projects governed by FIDIC contract terms, ensuring adherence to contractual obligations.",
+        description: "In our roles, we have acted as contract managers for projects governed by FIDIC contract terms, ensuring adherence to contractual obligations. We have also overseen construction supervision and capacity building contracts on a day-to-day basis, governed by PRAG contract conditions for various infrastructure projects.",
         color: "bg-green-500",
         image: "/assets/images/about-us-img2.jpg"
     },
@@ -19,13 +19,13 @@ const aboutCardData = [
     },
     {
         title: "Past Work",
-        description: "We have played a key role in the design, construction, commissioning, and operation of municipal water/wastewater treatment plants.",
+        description: "We have played a key role in the design, construction, commissioning, and operation of municipal water/wastewater treatment plants. ",
         color: "bg-orange-500",
         image: "/assets/images/about-us-img4.jpg"
     },
     {
         title: "Procurement",
-        description: "We possess comprehensive expertise in procurement for both international and local projects, including tasks such as preparing tender documents and evaluating tenders.",
+        description: "We possess comprehensive expertise in procurement for both international and local projects, includes tasks such as preparing tender documents, terms of reference, and evaluating tenders both administratively and technically.",
         color: "bg-red-500",
         image: "/assets/images/about-us-img5.jpg"
     },
